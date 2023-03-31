aa166a47caf0d699b6a13d22412b32a61ea35bb0
Mayank Jain <mayankjainx404@gmail.com>
2023-03-31 21:55:41 +0530

    Fixed ether transfer on sellorder function

--- contracts/stock.sol
+++ contracts/stock.sol
@@ -11,11 +11,11 @@ struct Holder{
 contract Stock{
     address private owner;
     address public contractAddress;
-    // uint public unsold_amount;
+    uint public unsold_amount;
     uint public amount = 0;
-    // uint public sellOrders = 0;
-    // uint public buyOrders = 0;
-    // string public companyName;
+    uint public sellOrders = 0;
+    uint public buyOrders = 0;
+    string public companyName;
     uint public currentPrice = 1;
     address[] transactions;
     mapping(address => uint) public holderIndex;
@@ -28,6 +28,9 @@ contract Stock{
     //     currentPrice = _currentPrice;
     // }
     event something(uint val);
+    event printBool(bool val);
+    event printString(string val);
+
 
     constructor(){
         contractAddress = address(this);
@@ -35,8 +38,12 @@ contract Stock{
             trader_address: 0x0000000000000000000000000000000000000000,
             amount: 0
         });
-        amount = 1;
+        amount = 600;
         holders.push(emptyHolder);
+    }
+
+    receive() payable external {
+        emit printString("Received the amount");
     }
 
 
@@ -55,27 +62,32 @@ contract Stock{
     }
 
 
-    function buyOrder(address _address) public payable  {
+    function buyOrder() public payable  {
         uint valueSent = msg.value/1000000000000000000;
         emit something(valueSent);
         require(valueSent == currentPrice);
-        payable(address(this)).send(currentPrice);
-        Holder memory updatedHolder = Holder({
-            trader_address: _address,
-            amount: 1
-        });
+        bool sent = payable(address(this)).send(currentPrice);
+        require(sent, "Not able to receive payment ethers");
+        emit printBool(sent);
+        address _address = msg.sender;
+        
 
         if(holderIndex[_address] == 0){
+            Holder memory updatedHolder = Holder({
+                trader_address: _address,
+                amount: 1
+            });
             holderIndex[_address] = holders.length;
             holders.push(updatedHolder);
         }else{
-            updatedHolder.amount = holders[holderIndex[_address]].amount + 1;
-            holders[holderIndex[_address]] = updatedHolder;
+            holders[holderIndex[_address]].amount += 1;
         }
     }
 
-    function sellOrder(address _address) public payable {
-        payable(msg.sender).transfer(currentPrice);
+    function sellOrder() public payable {
+        address payable _address = payable(msg.sender);
+        bool sent = _address.send(currentPrice*1 ether);
+        require(sent, "Was not able to send the ethers, reverted");
         uint index = holderIndex[_address];
         require(index != 0 && holders[index].amount > 0 );
         holders[index].amount -= 1;
@@ -84,7 +96,6 @@ contract Stock{
     function checkSender() view public returns (address){
         return msg.sender;
     }
-
 
     function addTransaction(address _address) public {
         transactions.push(_address);
