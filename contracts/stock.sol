119b29da39e490664e3be19dcfa8d6f2842a2e13
Mayank Jain <mayankjainx404@gmail.com>
2023-03-31 10:25:42 +0530

    Updated Buy order function in Stock.sol

--- contracts/stock.sol
+++ contracts/stock.sol
@@ -1,40 +1,73 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.2 <0.9.0;
 
 
-struct holder{
+struct Holder{
     address trader_address;
     string trader_name;
     uint amount;
-    address[] transactions;
 }
 
 contract Stock{
     address private owner;
-    uint public unsold_amount;
-    uint public amount = 0;
-    uint public sellOrders = 0;
-    uint public buyOrders = 0;
-    string public companyName;
-    uint public currentPrice;
+    address public contractAddress;
+    // uint public unsold_amount;
+    // uint public amount = 0;
+    // uint public sellOrders = 0;
+    // uint public buyOrders = 0;
+    // string public companyName;
+    // uint public currentPrice;
+    address[] transactions;
+    mapping(address => uint) public holderIndex;
+    Holder[] public holders;
 
-    constructor(string memory _companyName, uint _amount, uint _currentPrice ){
-        owner = msg.sender;
-        companyName = _companyName;
-        amount = _amount;
-        currentPrice = _currentPrice;
+    // constructor(string memory _companyName, uint _amount, uint _currentPrice ){
+    //     owner = msg.sender;
+    //     companyName = _companyName;
+    //     amount = _amount;
+    //     currentPrice = _currentPrice;
+    // }
+
+    constructor(){
+        contractAddress = address(this);
+        Holder memory emptyHolder = Holder({
+            trader_address: 0x0000000000000000000000000000000000000000,
+            trader_name: "mayank",
+            amount: 123
+        });
+        holders.push(emptyHolder);
     }
-
 
 
     function pay(address _recepient) public payable {
         address payable conntractAddress = payable(_recepient);
         conntractAddress.transfer(msg.value);
+        
     }
 
     function getBalance() public view returns(uint){
         return address(this).balance;
+    }
+
+    function buyOrder(address _address, string memory _name, uint _amount) public {
+
+        Holder memory updatedHolder = Holder({
+            trader_address: _address,
+            trader_name: _name,
+            amount: _amount
+        });
+
+        if(holderIndex[_address] == 0){
+            holderIndex[_address] = holders.length;
+            holders.push(updatedHolder);
+        }else{
+            holders[holderIndex[_address]] = updatedHolder;
+        }
+    }
+
+    function addTransaction(address _address) public {
+        transactions.push(_address);
     }
 
 }
