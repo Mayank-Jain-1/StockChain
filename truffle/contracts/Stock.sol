22036af335288b0ea172e980462f566698daaf85
Mayank Jain <mayankjainx404@gmail.com>
2023-04-04 00:19:22 +0530

    Added Deploy page with front end and deploy contract with aguements

--- truffle/contracts/Stock.sol
+++ truffle/contracts/Stock.sol
@@ -2,13 +2,12 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-
-struct Holder{
+struct Holder {
     address trader_address;
     uint amount;
 }
 
-contract Stock{
+contract Stock {
     address private owner;
     address public contractAddress;
     uint public unsold_amount;
@@ -24,10 +23,11 @@ contract Stock{
     event printBool(bool val);
     event printString(string val);
 
-
-    constructor(){
+    constructor(string memory _companyName, uint _amount, uint _initialPrice) {
+        companyName = _companyName;
+        unsold_amount = _amount;
+        currentPrice = _initialPrice;
         contractAddress = address(this);
-        unsold_amount = 600;
         amountInPublic = 0;
         Holder memory emptyHolder = Holder({
             trader_address: 0x0000000000000000000000000000000000000000,
@@ -36,46 +36,46 @@ contract Stock{
         holders.push(emptyHolder);
     }
 
-    receive() payable external {
+    receive() external payable {
         emit printString("Received the amount");
     }
 
-    function getBalance() public view returns(uint){
+    function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function buyOrder() external payable  {
-        uint valueSent = msg.value/1000000000000000000;
+    function buyOrder() external payable {
+        uint valueSent = msg.value / 1000000000000000000;
         emit something(valueSent);
         require(valueSent == currentPrice);
         bool sent = payable(address(this)).send(currentPrice);
         require(sent, "Not able to receive payment ethers");
         emit printBool(sent);
         address _address = msg.sender;
-        
-        if(unsold_amount > 0){
+
+        if (unsold_amount > 0) {
             unsold_amount--;
             amountInPublic++;
         }
 
-        if(holderIndex[_address] == 0){
+        if (holderIndex[_address] == 0) {
             Holder memory updatedHolder = Holder({
                 trader_address: _address,
                 amount: 1
             });
             holderIndex[_address] = holders.length;
             holders.push(updatedHolder);
-        }else{
+        } else {
             holders[holderIndex[_address]].amount += 1;
         }
     }
 
     function sellOrder() external payable {
         address payable _address = payable(msg.sender);
-        bool sent = _address.send(currentPrice*1 ether);
+        bool sent = _address.send(currentPrice * 1 ether);
         require(sent, "Was not able to send the ethers, reverted");
         uint index = holderIndex[_address];
-        require(index != 0 && holders[index].amount > 0 );
+        require(index != 0 && holders[index].amount > 0);
         holders[index].amount -= 1;
     }
 }
