ad5d7d60ce7140d185b7b1700a58d63515a67361
Mayank Jain <mayankjainx404@gmail.com>
2023-04-04 00:20:47 +0530

    Added Deploy with front end and deploy arguements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Stocks from "./pages/Stocks";
+import Deploy from "./pages/Deploy";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Stocks />} />
+        <Route path="/" element={<Deploy />} />
+        <Route path="/stocks" element={<Stocks />} />
       </Routes>
     </BrowserRouter>
   );
